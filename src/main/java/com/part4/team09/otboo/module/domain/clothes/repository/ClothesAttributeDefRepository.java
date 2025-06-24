612e5dc2d78e264042b22bca2063b4801def0831
narone <buzzin2426@gmail.com>
2025-06-24 17:22:23 +0900

    fix : 스키마 및 entity 수정

--- src/main/java/com/part4/team09/otboo/module/domain/clothes/repository/ClothesAttributeDefRepository.java
+++ src/main/java/com/part4/team09/otboo/module/domain/clothes/repository/ClothesAttributeDefRepository.java
@@ -1,9 +1,10 @@
 package com.part4.team09.otboo.module.domain.clothes.repository;
 
+import com.part4.team09.otboo.module.domain.clothes.entity.ClothesAttributeDef;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface ClothesAttributeDefRepository extends JpaRepository<ClothesAttributeRepository, UUID> {
+public interface ClothesAttributeDefRepository extends JpaRepository<ClothesAttributeDef, UUID> {
 
 }
