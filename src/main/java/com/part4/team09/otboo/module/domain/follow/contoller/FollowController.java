8a8975e3d57c0e98b35140bd6f4c3996083e1110
Yeonkyung-Jeong <edaily0129@gmail.com>
2025-06-25 23:05:12 +0900

    Feature: 팔로잉 목록 조회 구현 - 서비스 페이징, 컨트롤러 작성 도중 커밋

--- src/main/java/com/part4/team09/otboo/module/domain/follow/contoller/FollowController.java
+++ src/main/java/com/part4/team09/otboo/module/domain/follow/contoller/FollowController.java
@@ -6,11 +6,10 @@ import com.part4.team09.otboo.module.domain.follow.service.FollowService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
+
+import java.util.UUID;
 
 @Slf4j
 @RestController
@@ -32,6 +31,19 @@ public class FollowController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(followDto);
+    }
+
+    // TODO: 팔로잉 리스트 조회
+    @GetMapping("/api/follows/followings")
+    public ResponseEntity<FollowDto> getFollowingList(
+            @RequestParam UUID followerId,
+            @RequestParam(required = false) String cursor,
+            @RequestParam(required = false) UUID idAfter,
+            @RequestParam Integer limit,
+            @RequestParam(required = false) String nameLike
+            ){
+
+
     }
 
 }
--- src/main/java/com/part4/team09/otboo/module/domain/follow/repository/FollowRepository.java
+++ src/main/java/com/part4/team09/otboo/module/domain/follow/repository/FollowRepository.java
@@ -1,9 +1,17 @@
 package com.part4.team09.otboo.module.domain.follow.repository;
 
 import com.part4.team09.otboo.module.domain.follow.entity.Follow;
+
+import java.util.List;
 import java.util.UUID;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FollowRepository extends JpaRepository<Follow, UUID> {
 
+    // 팔로잉 목록 조회: 처음부터
+    List<Follow> findAllByFollowerIdOrderByIdAtDesc(UUID followerId, Pageable pageable);
+
+    // 팔로잉 목록 조회: idAfter부터
+    List<Follow> findAllByFollowerIdAndIdLessThenOrderByIdAtDesc(UUID followerId, UUID idAfter, Pageable pageable);
 }
