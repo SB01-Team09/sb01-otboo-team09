a3935df7db36a6f81a0937464dbd8097f7b71ac5
김주언 <wndjs803@naver.com>
2025-06-25 16:48:40 +0900

    feat: 위치 외부 API 연동

--- src/main/java/com/part4/team09/otboo/module/domain/location/service/LocationService.java
+++ src/main/java/com/part4/team09/otboo/module/domain/location/service/LocationService.java
@@ -1,10 +1,38 @@
 package com.part4.team09.otboo.module.domain.location.service;
 
+import com.part4.team09.otboo.module.domain.location.dto.response.LocationApiResponse;
+import com.part4.team09.otboo.module.domain.location.entity.Location;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 @Service
 @RequiredArgsConstructor
 public class LocationService {
+
+  private static final String BASE_URL = "https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?";
+
+  private final RestClient restClient;
+
+  // longitude 경도 <-> 기상청 y, 카카오 x 127.xxx
+  // latitude 위도 <-> 기상청 x, 카카오 y 37.xxxx
+  public Location createLocation(double longitude, double latitude) {
+
+    String requestX = "x=" + String.valueOf(longitude);
+    String requestY = "y=" + String.valueOf(latitude);
+    String requestUrl = BASE_URL + requestX + "&" + requestY;
+
+    ResponseEntity<LocationApiResponse> response =
+      restClient.get()
+        .uri(requestUrl)
+        .header("Authorization", "KakaoAK 레스트 키")
+        .retrieve()
+        .toEntity(LocationApiResponse.class);
+
+    LocationApiResponse apiResponse = response.getBody();
+
+    return null;
+  }
 
 }
--- src/main/java/com/part4/team09/otboo/module/domain/weather/controller/WeatherController.java
+++ src/main/java/com/part4/team09/otboo/module/domain/weather/controller/WeatherController.java
@@ -1,6 +1,9 @@
 package com.part4.team09.otboo.module.domain.weather.controller;
 
+import com.part4.team09.otboo.module.domain.location.service.LocationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/weathers")
 public class WeatherController {
+
+  private final LocationService locationService;
+
+  @GetMapping("/location")
+  public ResponseEntity<String> getLocation() {
+    double x = 127.10459896729914;
+    double y = 37.40269721785548;
+    locationService.createLocation(x, y);
+    return null;
+  }
 
 }
