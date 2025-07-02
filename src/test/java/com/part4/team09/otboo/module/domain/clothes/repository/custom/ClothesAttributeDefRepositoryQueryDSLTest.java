e4e5620e9eba7bdede66bc542c81a8fd373addc6
narone <buzzin2426@gmail.com>
2025-07-02 16:43:16 +0900

    refactor: 사용하지 않는 import 문 제거

--- src/test/java/com/part4/team09/otboo/module/domain/clothes/repository/custom/ClothesAttributeDefRepositoryQueryDSLTest.java
+++ src/test/java/com/part4/team09/otboo/module/domain/clothes/repository/custom/ClothesAttributeDefRepositoryQueryDSLTest.java
@@ -13,8 +13,6 @@ import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
