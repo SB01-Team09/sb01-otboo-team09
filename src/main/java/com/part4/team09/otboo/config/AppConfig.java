31685b8bd95d6dc8a487650d625017b011b1bc78
Yeonkyung-Jeong <edaily0129@gmail.com>
2025-07-03 17:50:40 +0900

    refactor: 복합 캐싱 config, 의존성, yml 파일 설정
    # Conflicts:
    #	src/main/java/com/part4/team09/otboo/config/AppConfig.java

--- src/main/java/com/part4/team09/otboo/config/AppConfig.java
+++ src/main/java/com/part4/team09/otboo/config/AppConfig.java
@@ -1,11 +1,11 @@
 package com.part4.team09.otboo.config;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.retry.annotation.EnableRetry;
 
 @Configuration
-@EnableRetry
+@EnableCaching
 @EnableJpaAuditing
 public class AppConfig {
 
