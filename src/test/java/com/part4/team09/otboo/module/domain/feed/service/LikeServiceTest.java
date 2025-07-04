2012eb8855fcc94c3726cbf9404c1ff9fe0bc874
yxoni <psy337250@gmail.com>
2025-07-04 10:17:46 +0900

    test: LikeService.create 실패 테스트 케이스 작성

--- src/test/java/com/part4/team09/otboo/module/domain/feed/service/LikeServiceTest.java
+++ src/test/java/com/part4/team09/otboo/module/domain/feed/service/LikeServiceTest.java
@@ -1,7 +1,7 @@
 package com.part4.team09.otboo.module.domain.feed.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
@@ -10,12 +10,13 @@ import static org.mockito.Mockito.verify;
 
 import com.part4.team09.otboo.module.domain.feed.dto.AuthorDto;
 import com.part4.team09.otboo.module.domain.feed.dto.FeedDto;
-import com.part4.team09.otboo.module.domain.feed.dto.OotdDto;
 import com.part4.team09.otboo.module.domain.feed.entity.Feed;
+import com.part4.team09.otboo.module.domain.feed.exception.FeedNotFoundException;
 import com.part4.team09.otboo.module.domain.feed.mapper.FeedMapper;
 import com.part4.team09.otboo.module.domain.feed.repository.FeedRepository;
 import com.part4.team09.otboo.module.domain.feed.repository.LikeRepository;
 import com.part4.team09.otboo.module.domain.user.entity.User;
+import com.part4.team09.otboo.module.domain.user.exception.UserNotFoundException;
 import com.part4.team09.otboo.module.domain.user.repository.UserRepository;
 import com.part4.team09.otboo.module.domain.weather.entity.Weather;
 import com.part4.team09.otboo.module.domain.weather.repository.WeatherRepository;
@@ -92,6 +93,35 @@ class LikeServiceTest {
       assertThat(result).isEqualTo(feedDto);
       verify(likeRepository).save(any());
     }
-  }
 
+    @Test
+    @DisplayName("좋아요 생성 실패 - 존재하지 않는 피드 ID")
+    void create_like_throwsFeedNotFoundException_whenFeedDoseNotExist() {
+      // given
+      UUID nonExistFeedId = UUID.randomUUID();
+      UUID userId = UUID.randomUUID();
+
+      given(feedRepository.findById(nonExistFeedId)).willReturn(Optional.empty());
+
+      // when & then
+      assertThrows(FeedNotFoundException.class,
+          () -> likeService.create(userId, nonExistFeedId));
+    }
+
+    @Test
+    @DisplayName("좋아요 생성 실패 - 존재하지 않는 유저 ID")
+    void create_like_throwsUserNotFoundException_whenUserDoseNotExist() {
+      // given
+      UUID feedId = UUID.randomUUID();
+      Feed mockFeed = mock(Feed.class);
+      UUID nonExistUserId = UUID.randomUUID();
+
+      given(feedRepository.findById(feedId)).willReturn(Optional.of(mockFeed));
+      given(userRepository.findById(nonExistUserId)).willReturn(Optional.empty());
+
+      // when & then
+      assertThrows(UserNotFoundException.class,
+          () -> likeService.create(nonExistUserId, feedId));
+    }
+  }
 }
