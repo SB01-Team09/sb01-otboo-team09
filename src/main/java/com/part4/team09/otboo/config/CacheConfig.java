e9cb35b922285d951c091da49ba6ebf618e6b70c
Yeonkyung-Jeong <edaily0129@gmail.com>
2025-07-07 10:42:35 +0900

    refactor: redis bean등록 프로필 설정 제거

--- src/main/java/com/part4/team09/otboo/config/CacheConfig.java
+++ src/main/java/com/part4/team09/otboo/config/CacheConfig.java
@@ -3,7 +3,6 @@ package com.part4.team09.otboo.config;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,7 +12,6 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
 
-@Profile("prod")
 @Configuration
 public class CacheConfig {
 
