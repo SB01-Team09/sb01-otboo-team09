0ce1d19592ead41f2a35ae649ccc482684ff6682
narone <buzzin2426@gmail.com>
2025-07-10 11:22:26 +0900

    refactor : 의상 속성 정의 dto 어셈블러 구현

---
+++ src/main/java/com/part4/team09/otboo/module/domain/clothes/assembler/ClothesAttributeDefDtoAssembler.java
@@ -0,0 +1,40 @@
+package com.part4.team09.otboo.module.domain.clothes.assembler;
+
+import com.part4.team09.otboo.module.domain.clothes.dto.data.ClothesAttributeDefDto;
+import com.part4.team09.otboo.module.domain.clothes.entity.ClothesAttributeDef;
+import com.part4.team09.otboo.module.domain.clothes.entity.SelectableValue;
+import com.part4.team09.otboo.module.domain.clothes.mapper.ClothesAttributeDefMapper;
+import com.part4.team09.otboo.module.domain.clothes.service.SelectableValueService;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ClothesAttributeDefDtoAssembler {
+
+  private final SelectableValueService selectableValueService;
+
+  private final ClothesAttributeDefMapper clothesAttributeDefMapper;
+
+  public List<ClothesAttributeDefDto> assemble(List<ClothesAttributeDef> defs) {
+
+    List<UUID> defIds = defs.stream()
+        .map(ClothesAttributeDef::getId)
+        .toList();
+
+    Map<UUID, List<SelectableValue>> valueMap = selectableValueService.findAllByAttributeDefIdIn(
+            defIds).stream()
+        .collect(Collectors.groupingBy(SelectableValue::getAttributeDefId));
+
+    return defs.stream().map(
+            def -> clothesAttributeDefMapper.toDto(def.getId(), def.getName(),
+                valueMap.get(def.getId()).stream()
+                    .map(SelectableValue::getItem)
+                    .toList()))
+        .toList();
+  }
+}
