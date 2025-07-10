c788a53f6e54964e4ff20143ea0d4f68ef99e585
yerimYeon <99879845+yinneu@users.noreply.github.com>
2025-07-10 14:20:15 +0900

    fix: MissingRequestCookieException 해결 및 예외 처리

--- src/main/java/com/part4/team09/otboo/module/domain/auth/contoller/AuthController.java
+++ src/main/java/com/part4/team09/otboo/module/domain/auth/contoller/AuthController.java
@@ -2,10 +2,13 @@ package com.part4.team09.otboo.module.domain.auth.contoller;
 
 import com.part4.team09.otboo.module.common.security.AuthCookieNames;
 import com.part4.team09.otboo.module.common.security.jwt.GeneratedToken;
+import com.part4.team09.otboo.module.domain.auth.exception.AuthenticationRequiredException;
+import com.part4.team09.otboo.module.domain.auth.exception.MissingRefreshTokenException;
 import com.part4.team09.otboo.module.domain.auth.service.AuthService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.CookieValue;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
@@ -30,8 +34,13 @@ public class AuthController {
   // access 토큰 조회
   @GetMapping("/me")
   public ResponseEntity<String> getAccessToken(
-    @CookieValue(AuthCookieNames.REFRESH_TOKEN_COOKIE_NAME) String refreshToken
+    @CookieValue(name = AuthCookieNames.REFRESH_TOKEN_COOKIE_NAME, required = false) String refreshToken
   ) {
+    // refresh token 이 없는 경우 로그인 요구 예외
+    if (refreshToken == null || refreshToken.isBlank()) {
+      throw AuthenticationRequiredException.noDetail();
+    }
+
     String accessToken = authService.getAccessTokenByRefreshToken(refreshToken);
     return ResponseEntity.ok(accessToken);
   }
@@ -39,9 +48,13 @@ public class AuthController {
   // refresh 토큰 재발급
   @PostMapping("/refresh")
   public ResponseEntity<String> refreshTokens(
-    @CookieValue(AuthCookieNames.REFRESH_TOKEN_COOKIE_NAME) String refreshToken,
+    @CookieValue(name = AuthCookieNames.REFRESH_TOKEN_COOKIE_NAME, required = false) String refreshToken,
     HttpServletResponse response
   ) {
+    if (refreshToken == null || refreshToken.isBlank()) {
+      throw MissingRefreshTokenException.noDetail();
+    }
+
     GeneratedToken generatedToken = authService.refreshTokens(refreshToken);
 
     Cookie refreshCookie = new Cookie(AuthCookieNames.REFRESH_TOKEN_COOKIE_NAME,
