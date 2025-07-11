ddb4642d718c42343b1b4b603a82dbae2593d910
yxoni <psy337250@gmail.com>
2025-07-11 16:49:41 +0900

    feat: NotificationDto 생성

---
+++ src/main/java/com/part4/team09/otboo/module/domain/notification/dto/NotificationDto.java
@@ -0,0 +1,16 @@
+package com.part4.team09.otboo.module.domain.notification.dto;
+
+import com.part4.team09.otboo.module.domain.notification.entity.Notification.Level;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record NotificationDto(
+    UUID id,
+    LocalDateTime createdAt,
+    UUID receiverId,
+    String title,
+    String content,
+    Level level
+) {
+
+}
