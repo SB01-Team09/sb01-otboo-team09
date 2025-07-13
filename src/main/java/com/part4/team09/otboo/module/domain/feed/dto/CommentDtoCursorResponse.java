9a48e384ea6d769b5b83595ae7f11826ea6156b0
Yeonkyung-Jeong <edaily0129@gmail.com>
2025-07-13 16:16:12 +0900

    feature: 피드 댓글 목록 조회 쿼리, 서비스 구현

---
+++ src/main/java/com/part4/team09/otboo/module/domain/feed/dto/CommentDtoCursorResponse.java
@@ -0,0 +1,18 @@
+package com.part4.team09.otboo.module.domain.feed.dto;
+
+import com.part4.team09.otboo.module.common.enums.SortDirection;
+import com.part4.team09.otboo.module.domain.user.dto.UserDto;
+
+import java.util.List;
+import java.util.UUID;
+
+public record CommentDtoCursorResponse(
+        List<CommentDto> data,
+        String nextCursor,
+        UUID nextIdAfter,
+        boolean hasNext,
+        int totalCount,
+        String sortBy,
+        SortDirection sortDirection
+) {
+}
---
+++ src/main/java/com/part4/team09/otboo/module/domain/feed/repository/CommentRepositoryQueryDSL.java
@@ -0,0 +1,57 @@
+package com.part4.team09.otboo.module.domain.feed.repository;
+
+import com.part4.team09.otboo.module.domain.feed.entity.*;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+@RequiredArgsConstructor
+public class CommentRepositoryQueryDSL {
+
+    private final JPAQueryFactory queryFactory;
+
+    QComment comment = QComment.comment;
+
+    // 댓글 목록 조회
+    public List<Comment> getComments(UUID feedId, String cursor, UUID idAfter, int limit) {
+        return queryFactory
+                .selectFrom(comment)
+                .where(
+                        cursorCondition(cursor, idAfter)
+                )
+                .orderBy(comment.createdAt.asc())
+                .limit(limit)
+                .fetch();
+    }
+
+    // 댓글 개수
+    public int countComments(UUID feedId){
+        Long count = queryFactory
+                .select(comment.count())
+                .from(comment)
+                .where(comment.feedId.eq(feedId))
+                .fetchOne();
+        return count != null ? Math.toIntExact(count) : 0;
+    }
+
+
+    private BooleanExpression cursorCondition(String cursor, UUID idAfter) {
+        if (cursor == null) return null;
+        LocalDateTime decodedCursor = LocalDateTime.parse(cursor);
+
+        BooleanExpression condition = comment.createdAt.gt(decodedCursor);
+
+        if (idAfter != null) {
+            condition = condition.or(
+                    comment.createdAt.eq(decodedCursor).and(comment.id.gt(idAfter))
+            );
+        }
+        return condition;
+    }
+}
