2a16ab996e334fe3334ef9c58f95a7c93f3bd3a0
yerimYeon <99879845+yinneu@users.noreply.github.com>
2025-07-14 13:38:59 +0900

    feat: 프로필 수정, 비밀번호 변경 인가 설정

--- src/test/java/com/part4/team09/otboo/module/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/part4/team09/otboo/module/domain/user/controller/UserControllerTest.java
@@ -2,17 +2,27 @@ package com.part4.team09.otboo.module.domain.user.controller;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.part4.team09.otboo.module.common.security.CustomUserDetails;
+import com.part4.team09.otboo.module.domain.auth.dto.AuthUserDto;
+import com.part4.team09.otboo.module.domain.user.dto.ProfileDto;
 import com.part4.team09.otboo.module.domain.user.dto.UserDto;
+import com.part4.team09.otboo.module.domain.user.dto.UserDtoCursorResponse;
+import com.part4.team09.otboo.module.domain.user.dto.request.PasswordUpdateRequest;
+import com.part4.team09.otboo.module.domain.user.dto.request.ProfileUpdateRequest;
 import com.part4.team09.otboo.module.domain.user.dto.request.UserListRequest;
 import com.part4.team09.otboo.module.domain.user.dto.request.UserLockUpdateRequest;
 import com.part4.team09.otboo.module.domain.user.dto.request.UserRoleUpdateRequest;
@@ -26,6 +36,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -48,6 +59,63 @@ class UserControllerTest {
   UserDto userDto = new UserDto(userId, LocalDateTime.now(), "test@email.com", "name", Role.ADMIN,
     null, false);
 
+  @Test
+  @DisplayName("본인 계정일 때 프로필 수정 200")
+  void updateProfile_success() throws Exception {
+    // given
+    ProfileUpdateRequest updateRequest = mock(ProfileUpdateRequest.class);
+    ProfileDto response = mock(ProfileDto.class);
+
+    AuthUserDto authUserDto = new AuthUserDto(userId, "email", "name", false, Role.USER);
+    CustomUserDetails userDetails = CustomUserDetails.create(authUserDto);
+
+    when(userService.updateProfile(any(UUID.class), any(ProfileUpdateRequest.class),
+      isNull())).thenReturn(response);
+
+    MockMultipartFile jsonPart = new MockMultipartFile(
+      "request",
+      "",
+      "application/json",
+      objectMapper.writeValueAsBytes(updateRequest)
+    );
+
+    // when & then
+    mockMvc.perform(multipart("/api/users/{userId}/profiles", userId)
+        .file(jsonPart)
+        .with(user(userDetails))
+        .contentType(MediaType.MULTIPART_FORM_DATA)
+        .with(request -> {
+          request.setMethod("PATCH");
+          return request;
+        })
+        .with(csrf()))
+      .andExpect(status().isOk())
+      .andDo(print());
+  }
+
+  @Test
+  @DisplayName("본인 계정이 아닐 때 비밀번호 변경 403 인가 예외")
+  void updatePassword_shouldThrow_IsNotOwner() throws Exception {
+    // given
+    PasswordUpdateRequest request = mock(PasswordUpdateRequest.class);
+
+    UUID anotherUserId = UUID.randomUUID();
+    AuthUserDto authUserDto = new AuthUserDto(anotherUserId, "email", "name", false, Role.USER);
+    CustomUserDetails userDetails = CustomUserDetails.create(authUserDto);
+
+    doNothing().when(userService).updatePassword(any(UUID.class), any(PasswordUpdateRequest.class));
+
+    // when & then
+    mockMvc.perform(get("/api/users", userId)
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(request))
+        .with(user(userDetails))
+        .with(csrf()))
+      .andExpect(status().isForbidden())
+      .andDo(print());
+  }
+
+
   @Nested
   @DisplayName("유저 계정 목록 조회")
   class GetUsers {
@@ -58,8 +126,8 @@ class UserControllerTest {
     void getUsers_success() throws Exception {
       // given
       UserListRequest request = mock(UserListRequest.class);
-      when(userService.changeRole(any(UUID.class), any(UserRoleUpdateRequest.class))).thenReturn(
-        userDto);
+      UserDtoCursorResponse response = mock(UserDtoCursorResponse.class);
+      when(userService.getUsers(any(UserListRequest.class))).thenReturn(response);
 
       // when & then
       mockMvc.perform(get("/api/users", userId)
@@ -76,8 +144,8 @@ class UserControllerTest {
     void getUsers_shouldThrow_withUserRole() throws Exception {
       // given
       UserListRequest request = mock(UserListRequest.class);
-      when(userService.changeRole(any(UUID.class), any(UserRoleUpdateRequest.class))).thenReturn(
-        userDto);
+      UserDtoCursorResponse response = mock(UserDtoCursorResponse.class);
+      when(userService.getUsers(any(UserListRequest.class))).thenReturn(response);
 
       // when & then
       mockMvc.perform(get("/api/users", userId)
