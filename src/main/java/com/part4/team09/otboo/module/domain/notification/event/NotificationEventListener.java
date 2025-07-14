2286feee979a337210c1ed1f46d674c1bf6cad3b
yxoni <psy337250@gmail.com>
2025-07-14 14:14:54 +0900

    feat: handleFeedLikedEvent 메서드 작성

--- src/main/java/com/part4/team09/otboo/module/domain/notification/event/NotificationEventListener.java
+++ src/main/java/com/part4/team09/otboo/module/domain/notification/event/NotificationEventListener.java
@@ -36,8 +36,7 @@ public class NotificationEventListener {
   @EventListener
   public void handleClothesAttributeDefCreatedEvent(ClothesAttributeDefCreatedEvent event) {
     String title = "새로운 의상 속성이 추가되었어요.";
-    String content = String.format("내 의상에 [%s] 속성을 추가해보세요.",
-        event.name());
+    String content = String.format("내 의상에 [%s] 속성을 추가해보세요.", event.name());
 
     NotificationCreateAllRequest request = new NotificationCreateAllRequest(
         title,
@@ -52,8 +51,7 @@ public class NotificationEventListener {
   @EventListener
   public void handleClothesAttributeDefUpdatedEvent(ClothesAttributeDefUpdatedEvent event) {
     String title = "의상 속성이 변경되었어요.";
-    String content = String.format("[%s] 속성을 확인해보세요.",
-        event.name());
+    String content = String.format("[%s] 속성을 확인해보세요.", event.name());
 
     NotificationCreateAllRequest request = new NotificationCreateAllRequest(
         title,
@@ -64,7 +62,25 @@ public class NotificationEventListener {
     notificationService.createAll(request);
   }
 
-  // TODO: 내 피드에 좋아요 또는 댓글 등록
+  @Async
+  @EventListener
+  public void handleFeedLikedEvent(FeedLikedEvent event) {
+    String title = String.format("%s님이 내 피드를 좋아합니다.", event.username());
+    String content = event.feedContent();
+
+    NotificationCreateRequest request = new NotificationCreateRequest(
+        event.userId(),
+        title,
+        content,
+        Level.INFO
+    );
+
+    notificationService.create(request);
+  }
+
+  // TODO: 내 피드에 댓글 등록
+
+
   // TODO: 팔로우한 사용자가 피드를 등록
   // TODO: 다른 사용자가 나를 팔로우
   // TODO: DM 수신
