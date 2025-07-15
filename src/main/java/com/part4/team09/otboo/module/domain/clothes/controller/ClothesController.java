e6c13db20406069ca271dfa3cc846584d49dcfe3
narone <buzzin2426@gmail.com>
2025-07-15 17:50:38 +0900

    refactor : 인가 추가 및 로그를 더 자세히 보여주도록 수정

--- src/main/java/com/part4/team09/otboo/module/domain/clothes/controller/ClothesController.java
+++ src/main/java/com/part4/team09/otboo/module/domain/clothes/controller/ClothesController.java
@@ -1,5 +1,6 @@
 package com.part4.team09.otboo.module.domain.clothes.controller;
 
+import com.part4.team09.otboo.module.common.security.CustomUserDetails;
 import com.part4.team09.otboo.module.domain.clothes.dto.data.ClothesDto;
 import com.part4.team09.otboo.module.domain.clothes.dto.request.ClothesCreateRequest;
 import com.part4.team09.otboo.module.domain.clothes.dto.request.ClothesUpdateRequest;
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -34,55 +36,71 @@ public class ClothesController {
   // 의상 생성
   @PostMapping
   public ResponseEntity<ClothesDto> create(
+      @AuthenticationPrincipal CustomUserDetails userDetails,
       @RequestPart("request") @Valid ClothesCreateRequest request,
       @RequestPart(value = "image", required = false) MultipartFile image
   ) {
-    log.info("의상 생성 요청");
+    log.info("의상 생성 요청: name = {}, type = {}, attributeSize = {}, imageIsNull = {}",
+        request.name(), request.type(), request.attributes().size(), image == null);
 
-    ClothesDto response = clothesService.create(request, image);
+    UUID userId = userDetails.getId();
+    ClothesDto response = clothesService.create(userId, request, image);
 
-    log.info("의상 생성 응답: {}", HttpStatus.CREATED.value());
+    log.info("의상 생성 응답: name = {}, type = {}, attributeSize = {}, imageUrl = {}",
+        response.name(), response.type(), response.attributes().size(), response.imageUrl());
     return ResponseEntity.status(HttpStatus.CREATED).body(response);
   }
 
   // 의상 조회
   @GetMapping
   public ResponseEntity<ClothesDtoCursorResponse> findByCursor(
+      @AuthenticationPrincipal CustomUserDetails userDetails,
       @RequestParam(required = false) String cursor,
       @RequestParam(required = false) UUID idAfter,
       @RequestParam int limit,
       @RequestParam(required = false) ClothesType typeEqual,
       @RequestParam UUID ownerId
   ) {
-    log.info("의상 조회 요청");
+    log.info("의상 조회 요청: cursor = {}, idAfter = {}, limit = {}, typeEqual = {}",
+        cursor, idAfter, limit, typeEqual);
 
-    ClothesDtoCursorResponse response = clothesService.findByCursor(cursor, idAfter, limit, typeEqual,
-        ownerId);
+    UUID userId = userDetails.getId();
+    ClothesDtoCursorResponse response = clothesService.findByCursor(userId, cursor, idAfter, limit,
+        typeEqual, ownerId);
 
-    log.info("의상 조회 응답: {}", HttpStatus.OK.value());
+    log.info("의상 조회 응답: clothesListSize = {}, nextCursor = {}, nextIdAfter = {}, hasNext = {}, "
+        + "totalCount = {}, sortBy = {}, sortDirection = {}", response.data().size(), response.nextCursor(),
+        response.nextIdAfter(), response.hasNext(), response.totalCount(), response.sortBy(), response.sortDirection());
     return ResponseEntity.status(HttpStatus.OK).body(response);
   }
 
   // 의상 수정
   @PatchMapping("/{clothesId}")
   public ResponseEntity<ClothesDto> update(
+      @AuthenticationPrincipal CustomUserDetails userDetails,
       @PathVariable UUID clothesId,
       @RequestPart("request") @Valid ClothesUpdateRequest request,
       @RequestPart(value = "image", required = false) MultipartFile image
   ) {
-    log.info("의상 수정 요청");
+    log.info("의상 수정 요청: name = {}, type = {}, attributeSize = {}, imageIsNull = {}",
+        request.name(), request.type(), request.attributes().size(), image == null);
 
-    ClothesDto response = clothesService.update(clothesId, request, image);
+    UUID userId = userDetails.getId();
+    ClothesDto response = clothesService.update(userId, clothesId, request, image);
 
-    log.info("의상 수정 응답: {}", HttpStatus.OK.value());
+    log.info("의상 수정 응답: name = {}, type = {}, attributeSize = {}, imageUrl = {}",
+        response.name(), response.type(), response.attributes().size(), response.imageUrl());
     return ResponseEntity.status(HttpStatus.OK).body(response);
   }
 
   @DeleteMapping("/{clothesId}")
-  public ResponseEntity<Void> delete(@PathVariable UUID clothesId) {
+  public ResponseEntity<Void> delete(
+      @AuthenticationPrincipal CustomUserDetails userDetails,
+      @PathVariable UUID clothesId) {
     log.info("의상 삭제 요청");
 
-    clothesService.delete(clothesId);
+    UUID userId = userDetails.getId();
+    clothesService.delete(userId, clothesId);
 
     log.info("의상 삭제 응답: {}", HttpStatus.NO_CONTENT.value());
     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
