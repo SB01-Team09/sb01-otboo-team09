356b9969633abd5018c376a91a2da19e2002324c
yxoni <psy337250@gmail.com>
2025-07-15 11:31:15 +0900

    feat: SSE 환경 구축

---
+++ src/main/java/com/part4/team09/otboo/module/domain/notification/sse/SseService.java
@@ -0,0 +1,37 @@
+package com.part4.team09.otboo.module.domain.notification.sse;
+
+import java.io.IOException;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Service
+@RequiredArgsConstructor
+public class SseService {
+
+  @Value("${sse.timeout}")
+  private long timeout;
+
+  private final SseEmitterRepository sseEmitterRepository;
+
+  public SseEmitter connect(UUID receiverId) {
+    SseEmitter sseEmitter = new SseEmitter(timeout);
+
+    sseEmitter.onCompletion(() -> sseEmitterRepository.delete(receiverId, sseEmitter));
+    sseEmitter.onTimeout(() -> sseEmitterRepository.delete(receiverId, sseEmitter));
+    sseEmitter.onError(e -> sseEmitterRepository.delete(receiverId, sseEmitter));
+
+    try {
+      sseEmitter.send(SseEmitter.event().name("connect").data("connected"));
+    } catch (IOException e) {
+      sseEmitterRepository.delete(receiverId, sseEmitter);
+    }
+
+    sseEmitterRepository.save(receiverId, sseEmitter);
+
+    return sseEmitter;
+  }
+
+}
