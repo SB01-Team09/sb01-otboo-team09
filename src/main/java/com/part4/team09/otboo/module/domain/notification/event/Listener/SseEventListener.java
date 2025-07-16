3af957b36b49bda284acfc778fb324b6b3b69825
yxoni <psy337250@gmail.com>
2025-07-16 11:33:52 +0900

    refactor: SseEventListener에 @Async 추가

--- src/main/java/com/part4/team09/otboo/module/domain/notification/event/Listener/SseEventListener.java
+++ src/main/java/com/part4/team09/otboo/module/domain/notification/event/Listener/SseEventListener.java
@@ -4,6 +4,7 @@ import com.part4.team09.otboo.module.domain.notification.event.NotificationCreat
 import com.part4.team09.otboo.module.domain.notification.event.NotificationCreatedMultipleEvent;
 import com.part4.team09.otboo.module.domain.notification.sse.SseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -14,11 +15,13 @@ public class SseEventListener {
 
   private final SseService sseService;
 
+  @Async
   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
   public void handleNotificationCreatedEvent(NotificationCreatedEvent event) {
     sseService.send(event.notificationDto());
   }
 
+  @Async
   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
   public void handleNotificationCreatedMultipleEvent(NotificationCreatedMultipleEvent event) {
     sseService.sendToUsers(event.notificationDtos());
