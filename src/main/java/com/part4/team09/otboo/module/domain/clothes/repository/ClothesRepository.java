831a0852d5b53731495f2b631a79a4e3223433ed
narone <buzzin2426@gmail.com>
2025-07-18 15:48:49 +0900

    refactor : 불필요한 import 삭제

--- src/main/java/com/part4/team09/otboo/module/domain/clothes/repository/ClothesRepository.java
+++ src/main/java/com/part4/team09/otboo/module/domain/clothes/repository/ClothesRepository.java
@@ -3,7 +3,6 @@ package com.part4.team09.otboo.module.domain.clothes.repository;
 import com.part4.team09.otboo.module.domain.clothes.entity.Clothes;
 import com.part4.team09.otboo.module.domain.clothes.entity.Clothes.ClothesType;
 import com.part4.team09.otboo.module.domain.clothes.repository.custom.CustomClothesRepository;
-import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
