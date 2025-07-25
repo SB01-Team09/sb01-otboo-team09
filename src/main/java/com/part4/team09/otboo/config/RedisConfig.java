49595418fa0ad2d66c964e7c399432fe71bf6ad1
yxoni <psy337250@gmail.com>
2025-07-25 16:03:15 +0900

    chore: test.yml에 sse.redis 설정 추가

--- src/main/java/com/part4/team09/otboo/config/RedisConfig.java
+++ src/main/java/com/part4/team09/otboo/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.part4.team09.otboo.config;
 import com.part4.team09.otboo.module.domain.notification.sse.RedisSseDisconnectSubscriber;
 import com.part4.team09.otboo.module.domain.notification.sse.RedisSseSendSubscriber;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,6 +11,7 @@ import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 
 @Configuration
+@ConditionalOnProperty(name = "sse.redis.enabled", havingValue = "true", matchIfMissing = true)
 @RequiredArgsConstructor
 public class RedisConfig {
 
