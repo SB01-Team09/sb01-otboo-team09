23bb237b16bced262923c7f3aab6c665bd44b09f
yxoni <psy337250@gmail.com>
2025-07-30 00:47:23 +0900

    chore: 일부 클래스 커버리지 제외

--- src/test/java/com/part4/team09/otboo/module/domain/notification/event/listener/NotificationEventListenerTest.java
+++ src/test/java/com/part4/team09/otboo/module/domain/notification/event/listener/NotificationEventListenerTest.java
@@ -1,4 +1,4 @@
-package com.part4.team09.otboo.module.domain.notification.event.Listener;
+package com.part4.team09.otboo.module.domain.notification.event.listener;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
